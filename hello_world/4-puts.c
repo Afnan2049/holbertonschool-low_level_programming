fed568cc83b0b08e0b7e0b6a810f3c9ac9e319bc
root <alfaidi.afnan@gmail.com>
2025-10-13 08:59:45 +0000

    four

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
+
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
- */
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
