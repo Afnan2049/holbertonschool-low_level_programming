fd3e6ca36cd4696476362b0bc97b2a801e218bbb
root <alfaidi.afnan@gmail.com>
2025-10-21 08:14:28 +0000

    two

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mu1 - multiplies two integers
+ *@a: first integer
+ *@b: second integer
+ *
+ * Return: result of multiplication
+ */
+int mu1(int a, int b)
+{
+    return (a * b);
+}
