6160896ffeae3005a88c4097953a201899e9a2fb
root <alfaidi.afnan@gmail.com>
2025-10-22 10:29:51 +0000

    isac asimov

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-* _puts - prints a string followed by a new line to stdout
-* @str: pointer to the string
-*/
+ * _puts - prints a string followed by a new line to stdout
+ * @str: pointer to the string
+ */
 void _puts(char *str)
 {
-while (*str);
-{
-_putchar(*str);
-str++;
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
-_putchar('\n');
-}
+
