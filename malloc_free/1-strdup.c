68995b60c85912d83334e9ce9554cf755321acdb
root <alfaidi.afnan@gmail.com>
2025-11-03 14:23:38 +0000

    one

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,26 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * 	which contains a copy of the string given as a parameter
- * @str: string to duplicate
- *
- * Return: pointer to duplicated string, or NULL if str = NULL or insufficient memory
- */
+* _strdup - returns a pointer to a newly allocated space in memory,
+* 	which contains a copy of the string given as a parameter
+* @str: string to duplicate
+*
+* Return: pointer to duplicated string, or NULL if str = NULL or insufficient memory
+*/
 char *_strdup(char *str)
 {
-	char *duplicate;
 
-	if (str == NULL)
-		return (NULL);
+char *duplicate;
+unsigned int i, length = 0;
 
-	duplicate = malloc(strlen(str) + 1);
+/* Return NULL if str is NULL */
+if (str == NULL)
+return (NULL);
 
-	if (duplicate == NULL)
-		return (NULL);
 
-	strcpy(duplicate, str);
-	return (duplicate);
+/* Calculate the length of the string manually */
+while (str[length] != '\0')
+	length++;
+
+/* Allocate memory for duplicate (length + 1 for null terminator) */
+duplicate = malloc(sizeof(char) * (length + 1));
+
+/* Return Null if malloc fails */
+if (duplicate == NULL)
+	return (NULL);
+
+/* Copy each character manually */
+for (i = 0; i <= length; i++)
+	duplicate[i] = str[i];
+
+return (duplicate);
 }
