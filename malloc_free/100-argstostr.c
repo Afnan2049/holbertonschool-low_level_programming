5bab14d32ef4b2779f73b32a89556c481d47acaa
root <alfaidi.afnan@gmail.com>
2025-11-04 16:14:12 +0000

    muhammed ali 100

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -2,41 +2,41 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of the program
- * @ac: argument count
- * @av: argument vector (array of strings)
- *
- * Return: pointer to new string, or NULL if failure
- */
+* argstostr - concatenates all the arguments of the program
+* @ac: argument count
+* @av: argument vector (array of strings)
+*
+* Return: pointer to new string, or NULL if failure
+*/
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int i, j, k = 0;
-	int total_len = 0;
+char *str;
+int i, j, k = 0;
+int total_len = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++);
-		total_len += j + 1;
-	}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++);
+total_len += j + 1;
+}
 
-	str = malloc(sizeof(char) * (total_len + 1));
-	if (str == NULL)
-		return (NULL);
+str = malloc(sizeof(char) * (total_len + 1));
+if (str == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
-	}
-		str[k] = '\0';
-		return (str);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+str[k] = av[i][j];
+k++;
+}
+str[k] = '\n';
+k++;
+}
+str[k] = '\0';
+return (str);
 }
