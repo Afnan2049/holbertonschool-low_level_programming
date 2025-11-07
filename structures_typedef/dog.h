63de82f873ddbc48c09f9af63677b9f2cbf10f68
root <alfaidi.afnan@gmail.com>
2025-11-07 13:26:43 +0000

    zero

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Structure representing a dog
+* @name: Name of dog (string)
+* @age: Age of dog (floating point number)
+* @owner: Owner's name of the dog (string)
+*
+* Description: This structure defines a dog with its name, age and owner's name.*
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#enfif
