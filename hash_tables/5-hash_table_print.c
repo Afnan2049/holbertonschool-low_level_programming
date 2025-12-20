a8d36ab63bec09675b4809b913a42baa231336e2
root <alfaidi.afnan@gmail.com>
2025-12-20 15:12:48 +0000

    fixing betty style in 5-hash_table_print.c file

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -5,7 +5,7 @@
 * hash_table_print - Prints a hash table
 * @ht: The hash table to print
 *
-* Description: Prints key/value in the order they appear in the array of the hash table
+* Description: Prints key/value in the order they appear in array of hash table
 */
 void hash_table_print(const hash_table_t *ht)
 {
